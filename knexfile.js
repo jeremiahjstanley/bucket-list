53bbd71446e0428fd712cfedf982c0ac6215b9aa
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-09-04 12:16:12 -0600

    Add test environment

--- knexfile.js
+++ knexfile.js
@@ -13,22 +13,17 @@ module.exports = {
     },
     useNullAsDefault: true
   },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/bucketlist_test',
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
   },
-
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + `?ssl=true`,
